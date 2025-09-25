1977d7d01fab1ca6ff07894d5f5d2fa7594353f9
soyeonhwa2-boop <soyeonhwa2@gmail.com>
2025-09-25 18:17:53 +0900

    2025.09.25

--- eulyoo/js/js_main.js
+++ eulyoo/js/js_main.js
@@ -62,4 +62,24 @@ $('.visual .ctrl_btn .btn_play').on('click', function(){
     $(window).scroll(function(){
         scroll_chk()
     })
+
+    
+    //book swiper
+            const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+            slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+            spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+            breakpoints: {
+                769: {    /* 640px 이상일때 적용 */
+                    slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                    spaceBetween: 24,
+                },
+            },
+            //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+          
+            navigation: {
+                nextEl: '.book .btn_next',
+                prevEl: '.book .btn_prev',
+            },
+            
+        });//book swiper 종료
 })///맨끝
