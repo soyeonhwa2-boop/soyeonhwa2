d16bcba3529cf820bc754840af0ad0b2bc6839ae
soyeonhwa2-boop <soyeonhwa2@gmail.com>
2025-10-28 15:53:04 +0900

    2025.10.28....

--- kumho/js/common.js
+++ kumho/js/common.js
@@ -84,5 +84,25 @@ $(document).ready(function(){
     $(window).scroll(function(){
         scroll_chk()//스크롤 할때마다 실행
     })
+
+
+    /* family_site 열고 닫기
+    footer .f_util.family_site .family_open 열기를 클릭하면
+    footer .f_util.family_site에 open 클래스를 추가
+    footer .f_util .family_site .family_close 닫기를 클릭하면*/
+    $('footer .f_util .family_site .family_open').on('click', function(){
+        $('footer .f_util .family_site').addClass('open')
+        $('footer .f_util .family_site .family_wrap').slideDown()
+    })
+    $('footer .f_util .family_site .family_close').on('click', function(){
+        $('footer .f_util .family_site').removeClass('open')
+        $('footer .f_util .family_site .family_wrap').slideUp()
+    })
+
+    $('footer .f_util .top').on('click', function(){
+        $('html, body').animate({
+            scrollTop : 0
+        }, 500)
+    })
 })//맨끝
 
